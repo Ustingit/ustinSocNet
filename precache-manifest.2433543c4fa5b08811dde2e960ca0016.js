13858b49a6af9ca3246ffd3d30421622a0667905
Ustin <ustinx@mail.ru>
2020-02-01 21:27:06 +0300

    first build

---
+++ precache-manifest.2433543c4fa5b08811dde2e960ca0016.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bebecb956ef64f67978886942232f8",
+    "url": "/Ustingit/ustinSocNet/index.html"
+  },
+  {
+    "revision": "88773d1597b8c124385d",
+    "url": "/Ustingit/ustinSocNet/static/css/3.f3377462.chunk.css"
+  },
+  {
+    "revision": "72ba8e8a10317ea58e2f",
+    "url": "/Ustingit/ustinSocNet/static/css/4.a087a0de.chunk.css"
+  },
+  {
+    "revision": "ce748bea23892ae52348",
+    "url": "/Ustingit/ustinSocNet/static/css/main.22111cea.chunk.css"
+  },
+  {
+    "revision": "0a48a85853d49403be07",
+    "url": "/Ustingit/ustinSocNet/static/js/2.9e7a0cb8.chunk.js"
+  },
+  {
+    "revision": "88773d1597b8c124385d",
+    "url": "/Ustingit/ustinSocNet/static/js/3.d23586f5.chunk.js"
+  },
+  {
+    "revision": "72ba8e8a10317ea58e2f",
+    "url": "/Ustingit/ustinSocNet/static/js/4.26b6bfc8.chunk.js"
+  },
+  {
+    "revision": "ce748bea23892ae52348",
+    "url": "/Ustingit/ustinSocNet/static/js/main.8e12c183.chunk.js"
+  },
+  {
+    "revision": "9952ba9ff4486950388d",
+    "url": "/Ustingit/ustinSocNet/static/js/runtime-main.0c2fd10b.js"
+  },
+  {
+    "revision": "6b0022312d41080436c52da571d5c697",
+    "url": "/Ustingit/ustinSocNet/static/media/avaDefault2.6b002231.png"
+  }
+]);
